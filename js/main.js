a92132d23ebbc2004f669c7b2dc4d0670830f66d
Rohan Malhotra <rohan.root@gmail.com>
2018-06-30 22:48:05 +0530

    added roles in some elements

--- js/main.js
+++ js/main.js
@@ -181,6 +181,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('role', 'button');
   more.href = DBHelper.urlForRestaurant(restaurant);
   container.append(more);
 
